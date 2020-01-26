8f0b3ea3124457af6abb969ca2c52abd7f95f931
Curt Manning <cmann4ski@yahoo.com>
2020-01-25 21:33:56 -0700

    Create Conftest

--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -3,30 +3,29 @@ from pages.home.login_page import LoginPage
 import unittest
 import pytest
 
+
+@pytest.mark.usefixtures("oneTimeSetUp", "setUp")
 class LoginTests(unittest.TestCase):
     """
     Make sure Install pytest-ordering and pytest in system terminal using the pip command.
 
-    py.test -v -s tests/home/login_tests.py
+    py.test -v -s tests/home/login_tests.py --browser firefox
+    py.test -v -s tests/home/login_tests.py --browser chrome
 
     """
-    baseURL = "https://letskodeit.teachable.com/"
-    driver = webdriver.Firefox(executable_path="C:\\Users\\CurtA\\selenium\\geckodriver.exe")
-    driver.maximize_window()
-    driver.implicitly_wait(3)
-    lp = LoginPage(driver)
+
+    @pytest.fixture(autouse=True)
+    def classSetup(self, oneTimeSetUp):
+        self.lp = LoginPage(self.driver)
 
     @pytest.mark.run(order=2)
     def test_validLogin(self):
-        self.lp.clearLoginFields()
         self.lp.login("test@email.com", "abcabc")
         result = self.lp.verifyLoginSuccessful()
         assert result == True
-        self.driver.quit()
 
     @pytest.mark.run(order=1)
     def test_invalidLogin(self):
-        self.driver.get(self.baseURL)
         self.lp.login("test@email.com", "abcabcabc")
         result = self.lp.verifyLoginFailed()
         assert result == True
