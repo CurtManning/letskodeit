17c7ae157908c3749fe941b24eb78e78256cd74c
curtmanning <curtmanning@ltgc.com>
2020-10-20 16:29:42 -0600

    next

--- base/webdriverfactory.py
+++ base/webdriverfactory.py
@@ -41,6 +41,7 @@ class WebDriverFactory():
 
     """
         Set chrome driver and iexplorer environment based on OS
+        
         chromedriver = "C:/.../chromedriver.exe"
         os.environ["webdriver.chrome.driver"] = chromedriver
         self.driver = webdriver.Chrome(chromedriver)
